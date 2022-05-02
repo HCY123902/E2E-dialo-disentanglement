67dec021a2e3c0ea5b7475fed69b73120cced53a
hcy <2320032466@qq.com>
2022-05-02 13:50:08 +0200

    Add the cluster

--- code/main.py
+++ code/main.py
@@ -139,6 +139,7 @@ if __name__ == "__main__":
     parser.add_argument('--glove_loc', type=str, default=constant.glove_path)
     parser.add_argument('--model_path', type=str, default='')
     parser.add_argument('--device', type=str, default='0')
+    parser.add_argument('--Kmeans_metric', type=str, choices=['silhouette', 'elbow', 'combined'], default='silhouette')
     args = parser.parse_args()
 
     os.environ['CUDA_VISIBLE_DEVICES'] = args.device
